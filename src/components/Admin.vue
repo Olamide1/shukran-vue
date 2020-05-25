6d400f18280c40f15904f634d71f25480d294403
Olamide Akomolafe <olamideakomolafe1234@gmail.com>
2020-05-25 21:47:35 +0300

    Fixed issues

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -58,7 +58,7 @@
                 <button class="uk-button" type="button">
                     info
                 </button>
-       <div uk-dropdown="mode: click; pos: bottom-right">
+       <div uk-dropdown="mode: click">
         <ul class="uk-nav uk-dropdown-nav uk-list uk-list-divider uk-panel-scrollable" >
      <li>Bank: {{user.bank}}</li>
     <li>Account name: {{user.account_name}}</li>
@@ -88,7 +88,7 @@
             <td>{{transaction.status}}</td>
             <td class="uk-inline">
                 <button class="uk-button uk-button-default" type="button">Info</button>
-                <div uk-dropdown="mode: click; pos: bottom-right">
+                <div uk-dropdown="mode: click">
                     <ul class="uk-nav uk-dropdown-nav uk-list uk-list-divider uk-panel-scrollable">
                 <li>Fullname: {{transaction.supporter_nickname}}</li>
              <li>Date: {{transaction.transaction_date}}</li>
