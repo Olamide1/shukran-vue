7c64ea069b2ce3f24f99251c50934e9a28a002c7
Olamide Akomolafe <olamideakomolafe1234@gmail.com>
2020-06-25 15:31:04 +0300

    update

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -68,8 +68,7 @@
     <div uk-alert>
     <a class="uk-alert-close" uk-close></a>
     <h3>Notice</h3>
-    <p>Hi {{username}} our 10% charge will take effect on payout requests from hence forth, 
-      this means if you place a payout request for &#x20a6;5000, we will be paying out &#x20a6;4500</p>
+    <p>Hi {{username}}, our 10% charge will take effect on payout requests from hence forth.</p>
 </div>
 
    <div class="uk-child-width-1-2@m uk-grid-match" uk-grid>
