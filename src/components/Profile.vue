823a987eade3faa9695e1b7bab3e5de1b6f60de2
Olamide Akomolafe <olamideakomolafe1234@gmail.com>
2020-07-07 20:23:39 +0300

    Messaging corrected

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -105,8 +105,7 @@
           <a class="uk-alert-close" uk-close></a>
           <h3>Hey {{username}},</h3>
           <p> Please fill your profile completely. There are <b>4 steps</b>, 
-            tap on "next" to find other other steps. Then tap the section and fill.
-            tap on arrows to get to the other steps.
+            tap on "arrows" to find other other steps. Then tap the section and fill.
           </p>
         </div>
       <div class="uk-card uk-width-2-3@m">
