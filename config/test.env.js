a5d51fb4832ad3dfb53ff94b7789a1dbf0abb049
wachukxs <nwachukwuossai@gmail.com>
2021-01-04 16:36:08 +0300

    more testing

--- config/test.env.js
+++ config/test.env.js
@@ -4,6 +4,6 @@ const merge = require('webpack-merge')
 const devEnv = require('./dev.env')
 
 module.exports = merge(devEnv, {
-  NODE_ENV: '"testing"',
+  NODE_ENV: '"test"',
   BASE_URL: '"https://shukran-staging-api.herokuapp.com/"'
 })
