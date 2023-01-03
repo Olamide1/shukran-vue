5fce9eae4f421faccccbd1ce02833119e04af237
wachukxs <nwachukwuossai@gmail.com>
2023-01-03 15:53:02 +0300

    design updates, code formatting.

--- src/components/Booking.vue
+++ src/components/Booking.vue
@@ -3,54 +3,82 @@
     <h1>Book me</h1>
 
     <div class="central">
-      <section class="">
-      <v-calendar
-        color="red"
-        @dayclick="onDayClick"
-        v-model="date"
-        title-position="right"
-      />
+      <section class="uk-padding">
 
-      <div class="uk-grid-small uk-grid-match" uk-grid>
+        <div class="uk-margin">
+          <v-calendar
+            color="red"
+            @dayclick="onDayClick"
+            v-model="date"
+            title-position="right"
+            is-expanded
+          />
+        </div>
 
+
+        <!-- should we filt uk-flex-between with uk-flex-around? -->
+        <div class="uk-margin">
+          <span class="uk-text-emphasis">Select time</span>
+          <div class="uk-grid-small uk-grid-match uk-flex-between" uk-grid>
           <!--
             Toggle will help https://getuikit.com/docs/toggle
 
             :uk-toggle="`target: #time-${index}; cls: uk-card-primary`"
           -->
-          <div @click="chooseTime(_time, index)"  v-for="(_time, index) in availableTimes" :key="index">
-            <div :id="`time-${index}`" class="uk-padding-small a-time uk-border-rounded uk-width-auto uk-card uk-card-default uk-card-body uk-grid-margin">
-              {{ _time }} {{ index }}
+          <div
+            @click="chooseTime(_time, index)"
+            v-for="(_time, index) in availableTimes"
+            :key="index"
+          >
+            <div
+              :id="`time-${index}`"
+              class="uk-padding-small a-time uk-border-rounded uk-width-auto uk-card uk-card-default uk-card-body uk-grid-margin"
+            >
+              {{ _time }}
             </div>
           </div>
+        </div>
+        </div>
+        
+      </section>
 
-      </div>
+      <section class="uk-padding">
+        <div class="uk-margin">
+          <input
+            name="fullname"
+            type="text"
+            class="uk-input uk-border-rounded"
+            placeholder="Fullname"
+            v-model="fullname"
+          />
+        </div>
 
-    </section>
+        <div class="uk-margin">
+          <input
+            name="email"
+            type="email"
+            class="uk-input uk-border-rounded"
+            placeholder="Email"
+            v-model="email"
+          />
+        </div>
 
-    <section>
-      <div class="uk-margin">
-        <input
-          name="fullname"
-          type="text"
-          class="uk-input"
-          placeholder="Fullname"
-          v-model="fullname"
-        />
-      </div>
+        <div class="uk-margin">
+          <textarea
+            name="message"
+            id="mgs"
+            class="uk-textarea uk-border-rounded"
+            v-model="message"
+            rows="5"
+            placeholder="Optional message"
+            aria-label="Message textarea"
+          ></textarea>
+        </div>
 
-      <div class="uk-margin">
-        <textarea
-          name="message"
-          id="mgs"
-          class="uk-textarea"
-          v-model="message"
-          rows="5"
-          placeholder="Optional message"
-          aria-label="Message textarea"
-        ></textarea>
-      </div>
-    </section>
+        <div class="uk-margin">
+          <button class="uk-button uk-button-default uk-border-rounded uk-align-right">Book</button>
+        </div>
+      </section>
     </div>
   </div>
 
@@ -74,7 +102,21 @@ export default {
       message: "",
       fullname: "",
       selectedTimeIndex: -1,
-      availableTimes: ["7:00 AM", "7:30 AM","7:00 AM", "7:30 AM","7:00 AM", "7:30 AM","7:00 AM", "7:30 AM","7:00 AM", "7:30 AM","7:00 AM", "7:30 AM",]
+      email: "",
+      availableTimes: [
+        "7:00 AM",
+        "7:30 AM",
+        "7:00 AM",
+        "7:30 AM",
+        "7:00 AM",
+        "7:30 AM",
+        "7:00 AM",
+        "7:30 AM",
+        "7:00 AM",
+        "7:30 AM",
+        "7:00 AM",
+        "7:30 AM",
+      ],
     };
   },
   methods: {
@@ -82,80 +124,88 @@ export default {
       console.log("selected date", day);
     },
     chooseTime(_time, index) {
-      
       /**
        * very first time, selectedIndexTime is -1.
        */
-      
+
       if (this.selectedTimeIndex == -1 || this.selectedTimeIndex == index) {
-        console.log('same or start');
+        console.log("same or start");
 
         // untoggle selectedIndex
-        UIkit.toggle();
+        let f = UIkit.toggle();
+        console.log("f", f);
 
         if (this.selectedTimeIndex == -1) {
-          this.selectedTimeIndex = index
+          this.selectedTimeIndex = index;
         } else {
-          this.selectedTimeIndex = -1
+          this.selectedTimeIndex = -1;
         }
-
       } else {
         // untoggle selectedIndex, and toggle index
-        console.log('not same');
+        console.log("not same");
         if (this.selectedTimeIndex > -1) {
-          console.log('untoggleing selectedIndex', this.selectedTimeIndex);
-          UIkit.toggle(`#time-${this.selectedTimeIndex}`, {
-            cls: 'uk-card-primary'
+          console.log("untoggleing selectedIndex", this.selectedTimeIndex);
+          let ff = UIkit.toggle(`#time-${this.selectedTimeIndex}`, {
+            cls: "uk-card-primary",
           }).toggle(); // the extra .toggle is a hot fix
+          console.log("ff", ff);
         }
 
-        UIkit.toggle(`#time-${index}`);
+        let fff = UIkit.toggle(`#time-${index}`);
+        console.log("fff", fff);
 
-        this.selectedTimeIndex = index
+        this.selectedTimeIndex = index;
       }
-      
 
-      console.log('selcected time', _time, index, 'this.selectedTimeIndex', this.selectedTimeIndex);
-    }
+      console.log(
+        "selcected time",
+        _time,
+        index,
+        "this.selectedTimeIndex",
+        this.selectedTimeIndex
+      );
+    },
   },
   mounted() {
-    UIkit.toggle('#time-0', {
-      cls: 'uk-card-primary'
+    // we need to shorten this
+    // all these initializes toggle on the elements
+    UIkit.toggle("#time-0", {
+      cls: "uk-card-primary",
     });
-    UIkit.toggle('#time-1', {
-      cls: 'uk-card-primary'
+    UIkit.toggle("#time-1", {
+      cls: "uk-card-primary",
     });
-    UIkit.toggle('#time-2', {
-      cls: 'uk-card-primary'
+    UIkit.toggle("#time-2", {
+      cls: "uk-card-primary",
     });
-    UIkit.toggle('#time-3', {
-      cls: 'uk-card-primary'
+    UIkit.toggle("#time-3", {
+      cls: "uk-card-primary",
     });
-    UIkit.toggle('#time-4', {
-      cls: 'uk-card-primary'
+    UIkit.toggle("#time-4", {
+      cls: "uk-card-primary",
     });
-    UIkit.toggle('#time-5', {
-      cls: 'uk-card-primary'
+    UIkit.toggle("#time-5", {
+      cls: "uk-card-primary",
     });
-    UIkit.toggle('#time-6', {
-      cls: 'uk-card-primary'
+    UIkit.toggle("#time-6", {
+      cls: "uk-card-primary",
     });
-    UIkit.toggle('#time-7', {
-      cls: 'uk-card-primary'
+    UIkit.toggle("#time-7", {
+      cls: "uk-card-primary",
     });
-    UIkit.toggle('#time-8', {
-      cls: 'uk-card-primary'
+    UIkit.toggle("#time-8", {
+      cls: "uk-card-primary",
     });
-    UIkit.toggle('#time-9', {
-      cls: 'uk-card-primary'
+    UIkit.toggle("#time-9", {
+      cls: "uk-card-primary",
     });
-    UIkit.toggle('#time-10', {
-      cls: 'uk-card-primary'
+    UIkit.toggle("#time-10", {
+      cls: "uk-card-primary",
     });
-    UIkit.toggle('#time-11', {
-      cls: 'uk-card-primary'
+    UIkit.toggle("#time-11", {
+      cls: "uk-card-primary",
     });
-  }
+  },
 };
 </script>
 
@@ -180,5 +230,9 @@ export default {
 
 .centeral * {
   margin: 0 auto;
+}
+
+section {
+
 }
 </style>
